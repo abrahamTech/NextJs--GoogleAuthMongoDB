5832ba38ed83c15853397ed044c6ad1863c34706
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-11-09 21:41:20 -0600

    feat(Navbar): add implementation of SigIn and SignOut to the Navbar button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,25 @@
+"use client";
+
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Navbar() {
+
+    const { status } = useSession();
+
     return (
         <div className="p-4 flex justify-between items-center shadow-md">
             <Link href={"/"} className="font-bold text-lg text-indigo-500">Google Auth</Link>
-            <button className="buttonSigIn bg-indigo-400 text-white px-6 py-2 rounded-md hover:bg-indigo-500 transition-all ease-in-out duration-300">Sign In</button>
+            
+            {status === "authenticated"
+                ? (<button onClick={() => signOut()} className="buttonSigIn bg-indigo-400 text-white px-6 py-2 rounded-md hover:bg-indigo-500 transition-all ease-in-out duration-300">
+                        Sign Out
+                    </button>)
+                : (<button onClick={() => signIn()} className="buttonSigIn bg-indigo-400 text-white px-6 py-2 rounded-md hover:bg-indigo-500 transition-all ease-in-out duration-300">
+                        Sign In
+                    </button>)
+            }
+
         </div>
     );
 }
